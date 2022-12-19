cd625f4e44d9b851c087ce3631b273355f48af5e
Raphael <okolonnaemeka2017@gmail.com>
2022-12-19 12:15:26 -0500

    Add js function in the hand bugger icon.

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ burger.addEventListener("click", function(){
     burger.classList.toggle("active");
     navMenu.classList.toggle("active");
 });
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
+    burger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))

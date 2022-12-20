11cc87fdbfc01e0d4fd3080402731efed3d90fd0
Ouail-01 <ouaillaarabi@gmail.com>
2022-12-20 13:47:52 +0100

    Fix the linters

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const burger = document.querySelector(".burger");
-const navMenu = document.querySelector("#nav-menu");
-const logo = document.querySelector(".logo");
-const body = document.querySelector("body");
-burger.addEventListener("click", function(){
-    burger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    logo.classList.toggle("active");
-    body.classList.toggle("active");
+const burger = document.querySelector('.burger');
+const navMenu = document.querySelector('#nav-menu');
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  logo.classList.toggle('active');
+  body.classList.toggle('active');
 });
-document.querySelectorAll(".nav-link").forEach((n) => n.addEventListener("click", () =>{
-    burger.classList.remove("active");
-    navMenu.classList.remove("active");
-    logo.classList.remove("active");
-    body.classList.remove("active");
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  burger.classList.remove('active');
+  navMenu.classList.remove('active');
+  logo.classList.remove('active');
+  body.classList.remove('active');
 }));

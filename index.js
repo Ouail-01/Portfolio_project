7df7e8c2d1bd83b3b2f7ae7d8dac4fccd64953ac
Ouail-01 <ouaillaarabi@gmail.com>
2022-12-21 14:57:13 +0100

    Update Js file

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ const cardsButton = document.querySelectorAll('.project-button');
 const myProjects = [
 
   {
-    title: 'Multi-Post Stories Gain+Glory',
+    title: 'Uber Navigation',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
     industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
     scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting
     industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
     printer took a galley of type and scrambled it 1960s with the relea`,
-    imagePop: 'img/card1.png',
-    technology: ['CodeKit', 'GitHub', 'javascript', 'Bootstrap', 'Terminal', 'Codepen'],
+    featuredImage: 'img/card1.png',
+    technology: ['Html', 'Css', 'javaScript', 'Bootstrap', 'GitHub', 'Codepen'],
     liveVersion: 'https://ouail-01.github.io/Portfolio_project/',
     sourceCode: 'https://github.com/Ouail-01',
   },
@@ -41,13 +41,13 @@ const myProjects = [
     industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
     printer took a galley of type and scrambled it 1960s with the relea`,
     featuredImage: 'img/card2.png',
-    technology: ['CodeKit', 'GitHub', 'javascript', 'Bootstrap', 'Terminal', 'Codepen'],
+    technology: ['CodeKit', 'GitHub', 'javaScript', 'Bootstrap', 'Terminal', 'Codepen'],
     liveVersion: 'https://ouail-01.github.io/Portfolio_project/',
     sourceCode: 'https://github.com/Ouail-01',
   },
 
   {
-    title: 'Multi-Post Stories Gain+Glory',
+    title: 'Multi-Post Stories',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
     industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
     scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting
@@ -60,43 +60,54 @@ const myProjects = [
   },
 
   {
-    title: 'Multi-Post Stories Gain+Glory',
+    title: 'Facebook 360',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
     industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
     scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting
     industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
     printer took a galley of type and scrambled it 1960s with the relea`,
     featuredImage: 'img/card4.png',
+    technology: ['Html', 'Ruby on rails', 'javascript', 'Css', 'Terminal', 'Codepen'],
+    liveVersion: 'https://ouail-01.github.io/Portfolio_project/',
+    sourceCode: 'https://github.com/Ouail-01',
+  },
+
+  {
+    title: 'Keeping track of hundreds of components',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+    scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting
+    industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+    printer took a galley of type and scrambled it 1960s with the relea`,
+    featuredImage: 'img/imgHolder2.png',
     technology: ['CodeKit', 'GitHub', 'javascript', 'Bootstrap', 'Terminal', 'Codepen'],
     liveVersion: 'https://ouail-01.github.io/Portfolio_project/',
     sourceCode: 'https://github.com/Ouail-01',
   },
 
   {
-    title: 'Multi-Post Stories Gain+Glory',
+    title: 'Keeping track of hundreds of components',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
     industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
     scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting
     industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
     printer took a galley of type and scrambled it 1960s with the relea`,
-    featuredImage: 'img/Img Placeholder.png',
-    technology: ['CodeKit', 'GitHub', 'javascript', 'Bootstrap', 'Terminal', 'Codepen'],
-    liveVersion: 'https://ouail-01.github.io/Portfolio_project/',
-    sourceCode: 'https://github.com/Ouail-01',
-  },
-
-  {
-    title: 'Multi-Post Stories Gain+Glory',
-    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-    scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting
-    industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
-    printer took a galley of type and scrambled it 1960s with the relea`,
-    featuredImage: 'img/Img Placeholder.png',
-    technology: ['CodeKit', 'GitHub', 'javascript', 'Bootstrap', 'Terminal', 'Codepen'],
+    featuredImage: 'img/imgHolder2.png',
+    technology: ['Ruby on rails', 'Css', 'javascript', 'Bootstrap', 'Terminal', 'GitHub'],
     liveVersion: 'https://ouail-01.github.io/Portfolio_project/',
     sourceCode: 'https://github.com/Ouail-01',
   },
 
 ];
 // const cardsButton = document.querySelectorAll('.project-button');
+
+cardsButton.forEach((item, i) => item.addEventListener('click', () => {
+  popupWindow.classList.toggle('active');
+  const project = myProjects[i];
+  const popContainer = document.querySelector('.popup-window');
+
+  const popupTitle = popContainer.querySelector('.popup-title');
+  popupTitle.textContent = project.title;
+  const popupDescription = popContainer.querySelector('.popup-description');
+  popupDescription.textContent = project.description;
+}));
